a2b6571674bcf07c59b616a9e31f7cd85a2e8579
MinZung <90918826+minza55113151@users.noreply.github.com>
2023-02-25 18:47:27 +0700

    Revert "Base awake of all command"
    This reverts commit 556f37e5a97b78a299c77231d4abfa6a4b1dc2ed.

--- Assets/Scripts/Instruction/Command.cs
+++ Assets/Scripts/Instruction/Command.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -13,18 +12,7 @@ public abstract class Command : MonoBehaviour
     protected SpriteRenderer spriteRenderer;
     protected Color originalColor;
     protected float clockPerExecute;
-
-    private TextMeshPro text;
-    private Color originalTextColor;
-    public virtual void Awake()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        originalColor = spriteRenderer.color;
-        clockPerExecute = Core.instance.clockPerExecute;
-
-        text = GetComponentInChildren<TextMeshPro>();
-        originalTextColor = text.color;
-    }
+    
     public abstract void Execute();
     public virtual void SetNextLinkedCommand(Command command)
     {
@@ -68,18 +56,5 @@ public abstract class Command : MonoBehaviour
             tmpCommand = tmpCommand.nextLinkedCommand;
         }
         return count;
-    }
-    public virtual void SetTransparent(bool isTransparent)
-    {
-        if (isTransparent)
-        {
-            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, ColorController.instance.onHoldedTransparent);
-            text.color = new Color(text.color.r, text.color.g, text.color.b, ColorController.instance.onHoldedTransparent);
-        }
-        else
-        {
-            spriteRenderer.color = originalColor;
-            text.color = originalTextColor;
-        }
     }
 }
